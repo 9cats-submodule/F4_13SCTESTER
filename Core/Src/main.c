063bbefe7284fa1c17568809abf9b480ee3555e5
HuangYi <123337671@qq.com>
2021-07-16 13:35:38 +0800

    :sparkles: 添加上下限频率的测量

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -97,20 +97,33 @@ s16 offset = 57;
 float RMS_COMPENSATE = 1134.0f;
 float SCAN_VPP[200];
 float SCAN_MAX;
+u32   SCAN_MAX_INDEX;
 u8 DATA_SHOW[51];
-//*100Hz 201个点
-u16 freqScanArr[50] =
-{
-   1,   2,   3,   4,   5,
-   6,   7,   8,   9,  10,
-  12,  14,  16,  18,  20,
-  26,  32,  38,  44,  50,
-  60,  70,  80,  90, 100,
- 120, 140, 160, 180, 200,
- 240, 280, 320, 360, 400,
- 480, 560, 640, 720, 800,
- 960,1120,1280,1440,1600,
-1760,1920,2080,2240,2400
+//*10Hz 116个点
+u16 freqScanArr[116] ={     0, //  0
+    0,    0,    0,    0,   10, // 100
+   12,   14,   16,   18,   20, // 200
+   22,   24,   26,   28,   30, // 300
+   32,   34,   36,   38,   40, // 400
+   42,   44,   46,   48,   50, // 500
+   54,   58,   62,   66,   70, // 700
+   76,   82,   88,   94,  100, // 1K
+  180,  260,  340,  420,  500, // 5K
+  600,  700,  800,  900, 1000, // 10K
+ 1100, 1200, 1300, 1400, 1500, // 15K
+ 1600, 1700, 1800, 1900, 2000, // 20K
+ 2200, 2400, 2600, 2800, 3000, // 30K
+ 3200, 3400, 3600, 3800, 4000, // 
+ 4200, 4400, 4600, 4800, 5000, //
+ 5200, 5400, 5600, 5800, 6000, //
+ 6200, 6400, 6600, 6800, 7000, //
+ 7200, 7400, 7600, 7800, 8000, //
+ 8400, 8800, 9200, 9600,10000, //
+10400,10800,11200,11600,12000, //
+12400,12800,13200,13600,14000, //
+14400,14800,15200,15600,16000, //
+16400,16800,17200,17600,18000, //
+18400,18800,19200,19600,20000  //
 };
 //-----------------------------标志---------------------------------------
 extern u8 SAMPLE_END_FLAG;    //采样结束标记
@@ -225,24 +238,22 @@ float SCAN_FFT_1K(u8 ch)
   return FFT_OUTPUT_REAL[32];
 }
 
-float SCAN_RMS(u8 ch)
+float SCAN_RMS(void)
 {
   u16 i;
   float sum=0;
 
-  Init_ADS8688(ch);            //只开启通道1
-  SAMPLE_POINT  = 64;         //采集100个点
-  HAL_Delay(2);
+  SAMPLE_POINT  = 2048;         //采集100个点
   HAL_TIM_Base_Start_IT(&htim1); //开启定时器
   while(!SAMPLE_END_FLAG);       //等待采样完成
   SAMPLE_END_FLAG = 0;           //采样标记清零
 
-  for(i=0;i<64;i++)
+  for(i=0;i<2048;i++)
   {
     sum+= (s32)BUF[i]+offset;
   }
 
-  return sum/64;
+  return sum/2048;
 }
 /* USER CODE END 0 */
 
@@ -311,6 +322,8 @@ int main(void)
 	/*正常工作，测输入输出阻抗，增益，幅频特性，失真度*/
 	while(1)
 	{
+	  SetTextValue(1,49,(u8*)"测量电路参数中.   ");
+
       //开启CH1获取值
 	  HAL_Delay(100);
 	  CH_VPP_VALUE[0] = SCAN_FFT_1K(0x01)*Svar.CH1_COMPENSATE/511;
@@ -320,6 +333,7 @@ int main(void)
 	  SetTextValue(0,41,str);
 #endif
 
+	  SetTextValue(1,49,(u8*)"测量电路参数中..  ");
 
 	  //开启CH2获取值
 	  HAL_Delay(100);
@@ -333,6 +347,7 @@ int main(void)
 	  SetTextValue(1,41,str);
 #endif
 
+	  SetTextValue(1,49,(u8*)"测量电路参数中... ");
 
 	  //开启CH3获取值
 	  HAL_Delay(110);
@@ -345,12 +360,14 @@ int main(void)
 //	  SetTextValue(0,43,str);
 #endif
 
+	  SetTextValue(1,49,(u8*)"测量电路参数中....");
 
 	  RELAY_NO;
+	  Init_ADS8688(0x10);
 	  HAL_Delay(300);
 	  //开启CH4获取值
 	  //CH_VPP_VALUE[3] = SCAN_FFT_1K(0x10)*Svar.CH1_COMPENSATE/511;
-	  CH_VPP_VALUE[3] = SCAN_RMS(0x10)*Svar.CH1_COMPENSATE/RMS_COMPENSATE;
+	  CH_VPP_VALUE[3] = SCAN_RMS()*Svar.CH1_COMPENSATE/RMS_COMPENSATE;
 #ifdef TFT
 	  //显示CH4_VPP
 
@@ -378,26 +395,87 @@ int main(void)
 	  SetTextValue(1,44,str);
 #endif
 
-
 	  RELAY_NO;
+	  Init_ADS8688(0x10);
 	  HAL_Delay(100);
 	  //扫频
-	  for(i=0;i<50;i++)
+	  for(i=0;i<116;i++)
 	  {
-		Out_freq(2, freqScanArr[i]*100);
-		HAL_Delay(100);
-		SCAN_VPP[i] = SCAN_RMS(0x10)*Svar.CH1_COMPENSATE/RMS_COMPENSATE;
+		if(freqScanArr[i] == 0) SCAN_VPP[i] = 0;
+		else {
+			Out_freq(2, freqScanArr[i]*10);
+			sprintf((char*)str,"扫频:%ld%%",i*100/116);
+			SetTextValue(1,49,str);
+			HAL_Delay(20);
+			SCAN_VPP[i] = SCAN_RMS()*Svar.CH1_COMPENSATE/RMS_COMPENSATE;
+		}
 	  }
-	  Out_freq(2, 1000);
-	  //找最大值
-	  arm_max_f32(SCAN_VPP, 200, &SCAN_MAX, &i);
+	  //找最大值 -- 和最大值对应的序列
+	  arm_max_f32(SCAN_VPP, 200, &SCAN_MAX, &SCAN_MAX_INDEX);
 	  DATA_SHOW[0] = 0;
-	  for(i=1;i<=50;i++)
+	  for(i=0;i<116;i++)
 	  {
-		DATA_SHOW[i] = SCAN_VPP[i-1] / SCAN_MAX * 222;
+		if(i == 5)
+		{
+		  DATA_SHOW[5] = SCAN_VPP[5] / SCAN_MAX * 255;
+		  DATA_SHOW[4] = DATA_SHOW[5] * 4 / 5;
+		  DATA_SHOW[3] = DATA_SHOW[5] * 3 / 5;
+		  DATA_SHOW[2] = DATA_SHOW[5] * 2 / 5;
+		  DATA_SHOW[1] = DATA_SHOW[5] * 1 / 5;
+		}
+		else if(i > 5)
+		{
+		  DATA_SHOW[i] = SCAN_VPP[i] / SCAN_MAX * 255;
+		}
 	  }
-	  GraphChannelDataAdd(1,51,1,(u8*)DATA_SHOW,400);
+	  GraphChannelDataAdd(1,51,1,(u8*)DATA_SHOW,116);
 
+
+
+	  {//查找下限频率
+		  u16 freq_a,freq_b,lower_freq;
+		  SetTextValue(1,49,(u8*)"测量上下限频率中.");
+		  for(i=0;i<SCAN_MAX_INDEX;i++)
+		  if(SCAN_VPP[i] < 0.7071067f*SCAN_MAX && SCAN_VPP[i+1] > 0.7071067f*SCAN_MAX) break;
+
+		  freq_a = freqScanArr[i]*100;freq_b = freqScanArr[i+1]*100;
+		  lower_freq = (freq_a+freq_b)/2;
+		  while(freq_a != lower_freq && freq_b != lower_freq)
+		  {
+		    Out_freq(2, lower_freq/10.0f);
+		    HAL_Delay(20);
+		    SCAN_VPP[i] = SCAN_RMS()*Svar.CH1_COMPENSATE/RMS_COMPENSATE;
+		    if(SCAN_VPP[i] < 0.7071067f*SCAN_MAX) freq_a=lower_freq;
+		    if(SCAN_VPP[i] >=0.7071067f*SCAN_MAX) freq_b=lower_freq;
+		    lower_freq = (freq_a+freq_b)/2;
+		  }
+		  sprintf((char*)str,"%.1fHz",lower_freq/10.0f);
+		  SetTextValue(1,45,str);
+	  }
+
+	  {//查找上限频率
+		  u16 freq_a,freq_b,lower_freq;
+		  SetTextValue(1,49,(u8*)"测量上上限频率中.");
+		  for(i=SCAN_MAX_INDEX;i<116;i++)
+		  if(SCAN_VPP[i] > 0.7071067f*SCAN_MAX && SCAN_VPP[i+1] < 0.7071067f*SCAN_MAX) break;
+
+		  freq_a = freqScanArr[i]*10;freq_b = freqScanArr[i+1]*10;
+		  lower_freq = (freq_a+freq_b)/2;
+		  while(freq_a != lower_freq && freq_b != lower_freq)
+		  {
+		    Out_freq(2, lower_freq);
+		    HAL_Delay(20);
+		    SCAN_VPP[i] = SCAN_RMS()*Svar.CH1_COMPENSATE/RMS_COMPENSATE;
+		    if(SCAN_VPP[i] < 0.7071067f*SCAN_MAX) freq_a=lower_freq;
+		    if(SCAN_VPP[i] >=0.7071067f*SCAN_MAX) freq_b=lower_freq;
+		    lower_freq = (freq_a+freq_b)/2;
+		  }
+		  sprintf((char*)str,"%dHz",lower_freq);
+		  SetTextValue(1,46,str);
+	  }
+
+	  Out_freq(2, 1000);
+	  HAL_Delay(100);
 	  //DEBUG用，调节DDS输出
       if(freq != L_freq)
       {
